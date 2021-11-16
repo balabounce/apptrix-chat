c398079b958a43d05fc33e8459437c5c81f27d57
Balabounce <yoba.number.one@gmail.com>
2021-11-16 19:48:23 +0300

    html css 99%

--- src/components/Dialog/Dialog.component.jsx
+++ src/components/Dialog/Dialog.component.jsx
@@ -11,7 +11,23 @@ const Dialog = () => {
                     <div className="dialog_container">
                         <div className="message_body message_body_companion">
                             <h5>Makaev Azim</h5>
-                            <span>Hi John, what do you think about corporate messengers?I know some new apps on UC market</span>
+                            <span>Hi John, what do you think about corporate messengers?<br/>I know some new apps on UC market</span>
+                            <div className="time_container">
+                                <span>
+                                    7:41 PM
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="message">
+                    <div className="avatar">
+                        <img src={avatar_icon} alt="avatar_companion" />
+                    </div>
+                    <div className="dialog_container">
+                        <div className="message_body message_body_companion">
+                            <h5>Makaev Azim</h5>
+                            <span>I did’t have time to study all unified communications market.<br/>The real issue is overload.</span>
                             <div className="time_container">
                                 <span>
                                     7:41 PM
@@ -23,12 +39,39 @@ const Dialog = () => {
             </div>
             
             <div className="me_container">
-                <div className="message_body message_body_me">
-                    <span>Hello World</span>
+                <div className="message"> 
+                    <div className="dialog_container">
+                        <div className="message_body message_body_me">
+                            <span>Hi First name, I need some time for creating study peport.<br/>3 hours for everything</span>
+                            <div className="time_container">
+                                <span>
+                                    7:41 PM
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="companion_container">
+                <div className="message">
+                    <div className="avatar">
+                        <img src={avatar_icon} alt="avatar_companion" />
+                    </div>
+                    <div className="dialog_container">
+                        <div className="message_body message_body_companion">
+                            <h5>Makaev Azim</h5>
+                            <span>Ok, just do it</span>
+                            <div className="time_container">
+                                <span>
+                                    7:41 PM
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-    
 }
 
 export default Dialog;
--- src/components/InputForm/InputForm.component.jsx
+++ src/components/InputForm/InputForm.component.jsx
@@ -1,7 +1,22 @@
+import enterSvg from '../../assets/icons/enter.svg';
+import enterEmptySvg from '../../assets/icons/emptyEnter.svg';
+import './InputForm.styles.scss';
+
 const InputForm = () => {
-    return <section className="container">
-            <input type="text" />
-    </section>
+    return <section className="input">
+            <div  className="input_container">
+                <form action="#" method='post'>
+                    <textarea  contentEditable className='textarea_input' type="text" placeholder='Enter text message' onChange={(event) => {
+                        const texArea = document.querySelector(`.${event.target.classList}`);
+                        texArea.style.cssText = 'height:auto; padding:0';
+                        texArea.style.cssText = 'height:' + texArea.scrollHeight + 'px';
+                    }} />
+                    <button type='submit'>
+                        <img src={enterEmptySvg} alt="submut_post" />
+                    </button>
+                </form>
+            </div>
+        </section>;
 }
 
 export default InputForm;
