9e20d4d22432c6a12ce85c5d1b3d00888091c518
Balabounce <yoba.number.one@gmail.com>
2021-11-17 13:00:55 +0300

    html css complete

--- src/components/InputForm/InputForm.component.jsx
+++ src/components/InputForm/InputForm.component.jsx
@@ -1,19 +1,29 @@
 import enterSvg from '../../assets/icons/enter.svg';
 import enterEmptySvg from '../../assets/icons/emptyEnter.svg';
 import './InputForm.styles.scss';
+import { useState } from 'react';
 
 const InputForm = () => {
+    const [inputStatus, setInputStatus] = useState(false);
+
     return <section className="input">
             <div  className="input_container">
                 <form action="#" method='post'>
-                    <textarea  contentEditable className='textarea_input' type="text" placeholder='Enter text message' onChange={(event) => {
+                    <textarea className='textarea_input' type="text" placeholder='Enter text message' onChange={(event) => {
                         const texArea = document.querySelector(`.${event.target.classList}`);
                         texArea.style.cssText = 'height:auto; padding:0';
                         texArea.style.cssText = 'height:' + texArea.scrollHeight + 'px';
+                        texArea.value.length > 0 ? setInputStatus(true) : setInputStatus(false);
                     }} />
-                    <button type='submit'>
-                        <img src={enterEmptySvg} alt="submut_post" />
+                    {inputStatus ? 
+                        <button>
+                            <img src={enterSvg} alt="submit_post" onClick={() => console.log('clicked')}/>
+                        </button>
+                    :
+                    <button>
+                        <img src={enterEmptySvg} alt="submit_post" onClick={() => console.log('clicked')}/>
                     </button>
+                    }
                 </form>
             </div>
         </section>;
